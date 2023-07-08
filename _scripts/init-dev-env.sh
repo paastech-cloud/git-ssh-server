8f90f47934a2e69ae713b3494e921de5c52579c9
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2023-07-08 19:16:11 +0200

    make(scripts): generate ssh key pair for server

--- _scripts/init-dev-env.sh
+++ _scripts/init-dev-env.sh
@@ -2,6 +2,12 @@
 
 rm -rf _scripts/output/client*
 
+rm -rf _scripts/output/server*
+
 mkdir -p _scripts/output/client
 
+mkdir -p _scripts/output/server
+
 ssh-keygen -t ed25519 -C "userA@user.fr" -f _scripts/output/client/id_ed25519 -q -N ""
+
+ssh-keygen -t ed25519 -C "git@paastech.fr" -f _scripts/output/server/id_ed25519 -q -N ""
